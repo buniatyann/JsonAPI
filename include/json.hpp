5e11b8de4c9ba45e58bd26b5f008f65b979e9336
buniatyann <narekbuniatyan.tt255-1@polytechnic.am>
2025-12-28 23:06:29 +0400

    restructured json.hpp

---
+++ include/json.hpp
@@ -0,0 +1,30 @@
+#ifndef JSON_API_HPP
+#define JSON_API_HPP
+
+#include "types/json_value.hpp"
+#include <string>
+
+class json {
+public:
+    json();
+    json(const std::string& file_path);
+    json(const json& other);
+    json(json&& other) noexcept;
+    json& operator=(const json& other);
+    json& operator=(json&& other) noexcept;
+
+    static json parse(const std::string& json_string);
+    static json object();
+    static json array();
+
+    json_value& get_json();
+    const json_value& get_json() const;
+
+    std::string get_context(int indent = -1) const;
+    void write_file(const std::string& file_path, int indent = 2) const;
+
+private:
+    json_value json_data_;
+};
+
+#endif // JSON_API_HPP
